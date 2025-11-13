e216eed2e1e43a1a807fd10e3c50986d5c83a12a
donirizqi <donirizqi74@gmail.com>
2025-11-13 22:09:33 +0700

    about-us

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,21 +1,25 @@
 @extends('layouts.master')
-@section('title', 'About Page')
-
+@section('title', 'About Us')
 
 @section('content')
-
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <h1></h1>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12 ">
-            <h1>About EduFun</h1>
+<div class="container py-5">
+    <div class="card shadow-sm border-0 mx-auto" style="max-width: 800px;">
+        <div class="card-body text-center p-5">
+            <h2 class="fw-bold mb-4">About EduFun</h2>
+            <p class="text-muted fs-5">
+                EduFun adalah perusahaan pendidikan berbasis teknologi asal Indonesia.
+                EduFun menyediakan layanan akses pendidikan dalam format tulisan berbahasa Indonesia
+                yang disajikan secara online melalui website.
+            </p>
+            <p class="text-muted fs-5 mt-4">
+                Hingga Juni 2024, EduFun memiliki lebih dari 10 ribu pengguna.
+                EduFun hadir sebagai bentuk revolusi dari pendidikan di Indonesia
+                dengan mengedepankan cara berpikir kritis, logis, rasional, dan sumber
+                pengetahuan sains yang terintegrasi terhadap semua mahasiswa IT di Indonesia.
+                EduFun bercita-cita mencetak generasi Indonesia yang memahami ilmu pengetahuan
+                dan cinta belajar.
+            </p>
         </div>
     </div>
 </div>
-
-
 @endsection
